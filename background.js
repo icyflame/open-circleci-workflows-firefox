1c4bbe48b099a6c5021205c27c4f3935fe243111
Siddharth Kannan <mail@siddharthkannan.in>
2019-08-24 17:26:55 +0900

    Refactor code; new functions; remove unnecessary icon declarations

--- background.js
+++ background.js
@@ -1,20 +1,56 @@
-allTabs = browser.tabs.query({
-});
+const defaultPageActionTitle = "Circle CI";
+const pageActionEligibleDomains = /^https:\/\/github.com\//;
 
-allTabs.then((tabs) => {
-  for (var i = 0; i < tabs.length; i++) {
-    tab = tabs[i];
-    browser.pageAction.setIcon({ tabId: tab.id, path: "icons/cutouts-60.png" });
-    browser.pageAction.setTitle({ tabId: tab.id, title: "Circle CI" });
-    browser.pageAction.show(tab.id);
+function isEligible(tab, newURL) {
+  const tabURL = tab.url;
+  const matchObj = tabURL && tabURL.match(pageActionEligibleDomains);
+  return matchObj && matchObj.length > 0 && newURL !== "";
+}
+
+function showPageAction(tab, newURL) {
+  const title = defaultPageActionTitle + " => " + newURL;
+  const tabId = tab.id;
+
+  browser.pageAction.setTitle({ tabId, title });
+  browser.pageAction.show(tabId);
+}
+
+function showPageActionIfEligible(tab) {
+  const newURL = getWorkflowsURL(tab);
+  if (isEligible(tab, newURL)) {
+    showPageAction(tab, newURL);
+  }
+}
+
+function getWorkflowsURL(tab) {
+  const tabURL = tab.url || "";
+  const accountRepoParts = tabURL.replace(pageActionEligibleDomains, "");
+  const components = accountRepoParts.split("/");
+
+  if (components.length >= 2) {
+    const account = components[0];
+    const repo = components[1];
+
+    if (account != "" && repo != "") {
+      const fullURL = "https://circleci.com/gh/" + account + "/" + repo;
+      return fullURL;
+    }
+  }
+
+  return "";
+}
+
+browser.pageAction.onClicked.addListener((tab) => {
+  const newURL = getWorkflowsURL(tab);
+  if (newURL !== "") {
+    browser.tabs.update({
+      url: newURL
+    });
   }
 });
 
-
-browser.pageAction.onClicked.addListener((tab) => {
-  console.log(tab);
-  browser.tabs.update({
-    url: "https://circleci.com",
-    active: false
-  });
+// Whenever a tab updates, check if it is eligible for this page action. If yes, show the page
+// action.
+browser.tabs.onUpdated.addListener((tabID, changeInfo, tab) => {
+  showPageActionIfEligible(tab);
 });
